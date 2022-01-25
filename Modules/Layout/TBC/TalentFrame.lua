42b0e2b0d1ee37327a455035d02b2f2897c0f1b2
Frederik Verhoef <frederikverhoef@gmail.com>
2022-01-25 15:53:47 +0000

    Fixed talent rank text parenting.

--- Modules/Layout/TBC/TalentFrame.lua
+++ Modules/Layout/TBC/TalentFrame.lua
@@ -11,4 +11,16 @@ function Layout:LayoutTalentFrame()
     _G.PlayerTalentFrameScrollFrame:Point("TOPRIGHT", -60, -77)
     _G.PlayerTalentFrameScrollFrame:Size(301, 332)
     _G.PlayerTalentFrameScrollFrameScrollBar:Point("TOPLEFT", _G.PlayerTalentFrameScrollFrame, "TOPRIGHT", 2, -16)
+
+	for i = 1, _G.MAX_NUM_TALENTS do
+		local talent = _G["PlayerTalentFrameTalent" .. i]
+		local rank = _G["PlayerTalentFrameTalent" .. i .. "Rank"]
+
+		if talent then
+            local border = talent:GetBorder()
+            if border then
+                rank:SetParent(border)
+            end
+		end
+	end
 end
